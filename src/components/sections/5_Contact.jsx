e86700e70b2e85aabf326a9c5b5c3515ad9fa522
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-08-07 20:39:55 +0800

    調整Contact組件的標題和連結的可訪問性標籤

--- src/components/sections/5_Contact.jsx
+++ src/components/sections/5_Contact.jsx
@@ -5,8 +5,10 @@ function Contact() {
     <section id="contact">
       <Hero bgc="#18212F">
         <div className="container-mid text-center">
-          <h2 className="text-4xl font-bold mb-8 h-15 bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent
-          max-sm:text-2xl max-sm:mb-4">
+          <h2
+            className="text-4xl font-bold mb-8 h-15 bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent
+          max-sm:text-2xl max-sm:mb-4"
+          >
             Let's Work Together
           </h2>
           <p className="text-xl text-gray-300 mb-12">
@@ -14,24 +16,21 @@ function Contact() {
             can bring your ideas to life.
           </p>
           <div className="grid grid-cols-3 gap-8 mb-12 max-sm:grid-cols-1">
-            <a href="mailto:ytsung99@gmail.com" target="_blank">
+            <a href="mailto:ytsung99@gmail.com" target="_blank" aria-label="Send Email" className="group">
               <div className="w-16 h-16 bg-gradient-to-br from-blue-500 to-purple-600 rounded-full flex items-center justify-center mx-auto mb-4 group-hover:scale-110 transform transition-all duration-200">
                 📧
               </div>
               <h3 className="font-semibold text-gray-300 mb-2">Email</h3>
               <p className="text-gray-400">ytsung99@gmail.com</p>
             </a>
-            <a href="https://line.me/ti/p/e125710020" target="_blank">
+            <a href="https://line.me/ti/p/e125710020" target="_blank"  aria-label="Add Line" className="group">
               <div className="w-16 h-16 bg-gradient-to-br from-green-500 to-blue-600 rounded-full flex items-center justify-center mx-auto mb-4 group-hover:scale-110 transform transition-all duration-200">
                 💼
               </div>
               <h3 className="font-semibold text-gray-300 mb-2">Line</h3>
               <p className="text-gray-400">line/榆</p>
             </a>
-            <a
-              href="https://github.com/TunaSung?tab=repositories"
-              target="_blank"
-            >
+            <a href="https://github.com/TunaSung?tab=repositories" target="_blank"  aria-label="Github" className="group">
               <div className="w-16 h-16 bg-gradient-to-br from-purple-500 to-pink-600 rounded-full flex items-center justify-center mx-auto mb-4 group-hover:scale-110 transform transition-all duration-200">
                 🐙
               </div>
