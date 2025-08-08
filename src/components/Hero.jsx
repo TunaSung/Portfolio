8d3cc6dd79a529cbc357381bdd283029ad4f1c12
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-08-08 14:51:03 +0800

    調整Hero組件的樣式以改善在2xl螢幕上的顯示效果

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 function Hero({children, bgc}){
     return(
-        <div className="w-full py-20 max-sm:py-10"
+        <div className="w-full py-20 max-sm:py-10 2xl:py-30"
         style={{backgroundColor: bgc}}>
             {children}
         </div>
